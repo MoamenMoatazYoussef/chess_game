61e9ab03f67e629378c0634d027ca9093097cc8b
Mo'men Moataz <momen_7421@hotmail.com>
2018-12-23 22:41:10 +0200

    Added knight's algorithm for check_move

--- Chess_game/knight.h
+++ Chess_game/knight.h
@@ -14,4 +14,23 @@ public:
 		_type = piece_type::Knight;
 		_player = _p;
 	}
+
+	bool check_move(short r2, short c2)
+	{
+		short r = get_row();
+		short c = get_col();
+
+		short Xsquared  = (r2 - r) ^ 2;
+		short Ysquared  = (c2 - c) ^ 2;
+		bool circle_eqn = ((Xsquared + Ysquared) == 5);
+		return circle_eqn;
+			//This is the algorithm I'm proud of the most!! <3
+			//------------------------------------------------
+			//The knight moves (1,2) or (2,1) or (2,-1) or (1,-2) or (-1,-2) or (-2,-1) or (-2,1) or (-1,2)
+			//So if we imagine the position of the knight as an origin point for x and y axis
+			//We can say that the knight only moves to points which satisfy the circle that passes
+			//through these 8 points
+			//Using any 3 of them we can get the equation of that circle as X^2 + Y^2 = 5
+			//Where center is (0,0) -current position of knight-, and radius = sqrt(5)
+	}
 };
