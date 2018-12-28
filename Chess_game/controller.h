e7b41230805105514cf2773a767850991c47251b
Moamen-Moataz <momen_7421@hotmail.com>
2018-12-28 17:20:54 +0200

    Moved functions from .h to .cpp for controller and input_screen

--- Chess_game/controller.h
+++ Chess_game/controller.h
@@ -1,4 +1,3 @@
-
 #pragma once
 #include "chess_map.h"
 #include "input_screen.h"
@@ -9,62 +8,11 @@ public:
 	controller();
 	~controller();
 
-	bool check_boundary(std::vector<short> move)
-	{
-		for (short i = move.size()/2; i < move.size(); i++) //Divided by 2 because we only check on the NEW position
-		{
-			bool within_boundaries = (move[i] <= 7) && (move[i] >= 0);
-			if (!within_boundaries)
-			{
-				_screen.input_error(1);
-				return false;
-			}
-		}
-		return true;
-	}
+	bool check_boundary(std::vector<short>);
 
-	void run_iteration()
-	{
-		std::vector<short> move = _screen.get_parse_send();		//Step 1: get input
+	void run_iteration();
 
-		if (move.size() > 4)										//Check that we have both moves
-		{
-			_screen.input_error(0);
-			return;
-		}
-		
-		if (!check_boundary(move))								//Step 2: check that requested moves are within boundaries
-		{
-			_screen.input_error(1);
-			return;
-		}
-		
-		if (!_map.check_current_player(move[0], move[1]))	//Step 3: check that the player is moving a piece that belongs to them
-		{
-			_screen.input_error(2);
-			return;
-		}
-
-		if (!_map.check_move(move[0], move[1], move[2], move[3]))
-		{
-			_screen.input_error(3);
-			return;
-		}
-
-		if (!_map.check_path(move[0], move[1], move[2], move[3]))
-		{
-			_screen.input_error(3);
-			return;
-		}
-
-		_map.move_piece(move[0], move[1], move[2], move[3]);
-	}
-
-	void run()
-	{
-		//while loop containing the run iteration
-
-	}
+	void run();
 
 private:
 	chess_map _map;
--- Chess_game/input_screen.cpp
+++ Chess_game/input_screen.cpp
@@ -17,3 +17,56 @@ input_screen::input_screen()
 input_screen::~input_screen()
 {
 }
+
+std::string input_screen::get_input()
+{
+	std::string input;
+	std::cout << "Your move! Enter the input in the form: A1 to B1" << std::endl;
+	std::getline(std::cin, input);
+	return input;
+}
+
+void input_screen::parse_string_by_delimiter(std::string s, std::string d, std::vector<std::string> output)
+{
+	size_t pos = 0;
+	std::string token;
+	while ((pos = s.find(d)) != std::string::npos) {
+		token = s.substr(0, pos);
+		output.push_back(token);
+		s.erase(0, pos + d.length());
+	}
+}
+
+std::vector<short> input_screen::get_parse_send()
+{
+	std::string input = get_input();
+	std::vector<std::string> input_split;
+
+	parse_string_by_delimiter(input, " ", input_split);
+
+	short old_row = row_map[input_split[0].substr(0, 1)];
+	short old_col = std::stoi(input_split[0].substr(1, 1)) - OFFSET_FOR_INDEX;
+
+	short new_row = row_map[input_split[2].substr(0, 1)];
+	short new_col = std::stoi(input_split[2].substr(1, 1)) - OFFSET_FOR_INDEX;
+
+	std::vector<short> final_move({ old_row, old_col, new_row, new_col });
+
+	return final_move;
+}
+
+void input_screen::input_error(short e)
+{
+	switch (e)
+	{
+	case(0) :
+		std::cout << "ERROR: a parameter is missing, please input TWO rows and TWO columns: r1, c1, r2, c2." << std::endl;
+		break;
+	case(1) :
+		std::cout << "ERROR: row/col is out of bounds." << std::endl;
+		break;
+	case(2) :
+		std::cout << "ERROR: trying to move other player's piece" << std::endl;
+		break;
+	}
+}
